3ca9e8b425022d7775737b2df8c4498690694bb1
Alireza Bamdad <alirezabamdad450@gmail.com>
2023-08-18 20:05:54 +0430

    final

---
+++ src/components/CourseCard.jsx
@@ -0,0 +1,48 @@
+function CourseCard({ course }) {
+  return (
+    <div className="course-item">
+      <div className="course-item__img">
+        <img src={course.imageUrl} alt={course.title} />
+      </div>
+      <div className="course-item__detail">
+        <CourseCardBody course={course} />
+        <CourseCardFooter course={course} />
+
+      </div>
+    </div>
+  );
+}
+
+export default CourseCard;
+
+function CourseCardBody({ course }) {
+  return (
+    <div className="course-item__body">
+      <div>
+        <p className="title">{course.title}</p>
+        <p className="dec">{course.description}</p>
+      </div>
+      <span className="rate">{course.rate}</span>
+    </div>
+  );
+}
+
+function CourseCardFooter({ course }) {
+  return (
+    <div className="course-item__footer">
+      <div className="tags">
+        {course.tags.map((t) => (
+          <span key={t} className="badge badge--secondary">
+            {t}
+          </span>
+        ))}
+      </div>
+      <div className="caption">
+        <div className="date">
+          {new Date(course.start).toLocaleDateString("en-Us")}
+        </div>
+        <span className="badge badge--primary">{course.status}</span>
+      </div>
+    </div>
+  );
+}
